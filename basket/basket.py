6e90f610480956f54b6e13879deddf3488c060e2
Jimbo Siona <jetigeenn@gmail.com>
2022-01-22 05:52:17 +0300

    creating add functionality to the basket

--- basket/basket.py
+++ basket/basket.py
@@ -11,3 +11,20 @@ class Basket:
         if 'skey' not in request.session:
             basket = self.session['skey'] = {}
         self.basket = basket
+
+    def add(self, product, qty):
+        """
+        Adding and updating the users basket session data
+        """
+        product_id = str(product.id)
+
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        else:
+            self.basket[product_id] = {'price': str(product.price), 'qty': qty}
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
