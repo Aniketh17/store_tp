5c44da67fdce2462586b1d460036d2bd8470ec0b
Jimbo Siona <jetigeenn@gmail.com>
2022-01-22 09:20:00 +0300

    Create view for profile edit

--- account/urls.py
+++ account/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
         template_name='account/registration/login.html',
         form_class=UserLoginForm
     ),
-     name='login'),
+         name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/account/login/'), name='logout'),
     path('register/', views.account_register, name='register'),
     path('activate/<slug:uidb64>/<slug:token>)/', views.account_activate, name='activate'),
@@ -40,4 +40,9 @@ urlpatterns = [
 
     # User dashboard
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('profile/edit/', views.edit_details, name='edit_details'),
+    path('profile/delete_user/', views.delete_user, name='delete_user'),
+    path('profile/delete_confirm/',
+         TemplateView.as_view(template_name="account/user/delete_confirm.html"),
+         name='delete_confirmation'),
 ]
