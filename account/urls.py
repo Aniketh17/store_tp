2428bc1d0eae3e7ad45245a8a4e700e0c155d54d
Jimbo Siona <jetigeenn@gmail.com>
2022-01-22 08:46:45 +0300

    Finishing the account activation template

--- account/urls.py
+++ account/urls.py
@@ -8,4 +8,7 @@ app_name = 'account'
 urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='/account/login/'), name='logout'),
     path('register/', views.account_register, name='register'),
+    path('activate/<slug:uidb64>/<slug:token>)/', views.account_activate, name='activate'),
+    # User dashboard
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,12 +1,21 @@
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 from .forms import RegistrationForm
+from .models import UserBase
 from .tokens import account_activation_token
+
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/user/dashboard.html')
 
 
 def account_register(request):
@@ -34,3 +43,18 @@ def account_register(request):
     else:
         register_form = RegistrationForm()
     return render(request, 'account/registration/register.html', {'form': register_form})
+
+
+def account_activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = UserBase.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, user.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('account:dashboard')
+    else:
+        return render(request, 'account/registration/activation_invalid.html')
