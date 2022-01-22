94190cfdcc6ae61c8a87da62229c5906f37d6c06
Jimbo Siona <jetigeenn@gmail.com>
2022-01-22 07:42:33 +0300

    creating the custom user model

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'store',
     'basket',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model
+AUTH_USER_MODEL = 'account.UserBase'
+LOGIN_REDIRECT_URL = '/account/dashboard'
+LOGIN_URL = '/account/login/'
