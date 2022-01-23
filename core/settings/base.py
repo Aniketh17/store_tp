b5a71f89233c599b5f51428f52789553d53bd460
Jimbo Siona <jetigeenn@gmail.com>
2022-01-23 22:41:13 +0300

    Multi-Product types database implementation

--- core/settings/base.py
+++ core/settings/base.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'account',
     'payment',
     'orders',
+
+    'mptt',
 ]
 
 MIDDLEWARE = [
--- store/views.py
+++ store/views.py
@@ -4,13 +4,13 @@ from .models import Category, Product
 
 
 def product_all(request):
-    products = Product.products.all()
+    products = Product.objects.all()
     return render(request, 'store/index.html', {'products': products})
 
 
 def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
-    products = Product.products.filter(category=category)
+    products = Product.objects.filter(category=category)
     return render(request, 'store/category.html', {'category': category, 'products': products})
 
 
